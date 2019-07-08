d7eea537e516f5712415d352b0ce9804b0f77c9f
NickP0is0n <nickchaywork@gmail.com>
2019-07-09 00:48:56 +0300

    Refactoring

--- src/main/java/me/NickP0is0n/Controller.java
+++ src/main/java/me/NickP0is0n/Controller.java
@@ -2,6 +2,7 @@ package me.NickP0is0n;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,9 +15,6 @@ import java.io.*;
 import java.util.Date;
 
 public class Controller {
-
-    private Student currentStudent;
-
     @FXML
     private TextField nameField;
 
@@ -46,16 +44,7 @@ public class Controller {
 
     @FXML
     void about(ActionEvent event) {
-        Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION); //Создание окна ошибки
-        aboutAlert.setTitle("About jTest Result Decryptor");
-        aboutAlert.setGraphic(new ImageView(new Image(Controller.class.getClassLoader().getResourceAsStream("logo.png"))));
-        aboutAlert.setHeaderText("jTest Result Decryptor");
-        aboutAlert.setContentText("Version 1.1 Beta 1\n\n" +
-                "jTest Result Decryptor is a program to decrypt the results of users tested in jTest Student.\n\n" +
-                "jTest Result Decryptor is a part of jTest software package.\n"+
-                "Source code licensed under BSD-3 Clause license. Feel free to use/copy/modify this package as long as you specifying the name of the author.\n" +
-                "Copyright (c) 2019, Nickolay Chaykovskyi All rights reserved.");
-        aboutAlert.showAndWait();
+        showInformationAboutApplication();
     }
 
     @FXML
@@ -64,48 +53,9 @@ public class Controller {
     }
 
     @FXML
-    void export(ActionEvent event) throws FileNotFoundException {
-        FileChooser chooser = new FileChooser(); //діалог збереження
-        chooser.setTitle("Save text file with results");
-        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files (.txt)", "*.txt")); //фильтр файлов
-        File taskFile = chooser.showSaveDialog(new Stage()); //показ диалога на отдельной сцене
-        boolean succesfullOpen = true;
-        if (taskFile != null) {
-            try {
-                taskFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-                succesfullOpen = false;
-                showError("An error occurred while creating the file!");
-            }
-            if (succesfullOpen)
-            {
-                PrintWriter out = new PrintWriter(taskFile);
-                out.println("jTest Exported Result");
-                out.println("jRes Generation 1");
-                out.println();
-                out.println("GENERAL INFO");
-                out.println("User surname: " + currentStudent.getSurName());
-                out.println("User name: " + currentStudent.getName());
-                out.println("Additional info: " + currentStudent.getGrade());
-                out.println();
-                out.println("TASK COMPLETION INFO");
-                out.println("Start time: " + currentStudent.getStartTime());
-                out.println("End time: " + currentStudent.getFinishTime());
-                out.println("Number of completed tasks: " + currentStudent.getTasksResults().size());
-                out.println("Number of passed tests: " + Integer.parseInt(doneTestField.getText()) + " out of " + currentStudent.getTasksResults().size() * 5);
-                for(int i = 0; i < currentStudent.getTasksResults().size(); i++)
-                {
-                    out.println();
-                    out.println("TASK #" + (i + 1));
-                    out.println("Tests passed: " + currentStudent.getDoneTasks()[i] + " out of 5");
-                }
-                out.println();
-                out.println("User results exported: " + new Date().toString());
-                out.close();
-            }
-        }
-
+    void export(ActionEvent event) {
+        final File targetFile = getFileFromSaveDialog("Save text file with results", "*.txt", "Text files (.txt)");
+        saveResultsToFile(targetFile);
     }
 
     @FXML
@@ -134,6 +84,105 @@ public class Controller {
             catch(Exception ex){
                 ex.printStackTrace();
                 showError("An error occurred while reading the file!");
+            }
+        }
+    }
+
+
+    private Student currentStudent;
+    private AppInfo appInfo = AppInfo.getInstance();
+
+    private void showInformationAboutApplication()
+    {
+        final Alert.AlertType alertType = Alert.AlertType.INFORMATION;
+        final String title = "About jTest Result Decryptor";
+        final Node graphic = new ImageView(new Image(Controller.class.getClassLoader().getResourceAsStream("logo.png")));
+        final String headerText = "jTest Result Decryptor";
+        final String contentText = appInfo.getVersion() + "\n\n" +
+                "jTest Result Decryptor is a program to decrypt the results of users tested in jTest Student.\n\n" +
+                "jTest Result Decryptor is a part of jTest software package.\n"+
+                "Source code licensed under BSD-3 Clause license. Feel free to use/copy/modify this package as long as you specifying the name of the author.\n" +
+                "Copyright (c) 2019, Nickolay Chaykovskyi All rights reserved.";
+
+        Alert infoAlert = makeGraphicalAlert(alertType, title, headerText, contentText, graphic);
+        showAlert(infoAlert);
+    }
+
+    private Alert makeAlert(Alert.AlertType alertType, String title, String headerText, String contentText) {
+        Alert targetAlert = new Alert(alertType);
+        targetAlert.setTitle(title);
+        targetAlert.setHeaderText(headerText);
+        targetAlert.setContentText(contentText);
+        return targetAlert;
+    }
+
+    private Alert makeGraphicalAlert(Alert.AlertType alertType, String title, String headerText, String contentText, Node graphic) {
+        Alert targetAlert = makeAlert(alertType, title, headerText, contentText);
+        targetAlert.setGraphic(graphic);
+        return targetAlert;
+    }
+
+    private void showAlert(Alert alert)
+    {
+        alert.showAndWait();
+    }
+
+    private FileChooser makeChooser(String title, String extension, String extensionDescription) {
+        FileChooser chooser = new FileChooser(); //діалог збереження
+        chooser.setTitle(title);
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(extensionDescription, extension)); //фильтр файлов
+        return chooser;
+    }
+
+    private File getFileFromSaveDialog(String title, String extension, String extensionDescription) //extension in *.ext format
+    {
+        FileChooser chooser = makeChooser(title, extension, extensionDescription);
+        return chooser.showSaveDialog(new Stage()); //показ диалога на отдельной сцене
+    }
+
+    private void printResults(File targetFile) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(targetFile)) {
+            out.println("jTest Exported Result");
+            out.println("jRes Generation 1");
+            out.println();
+            out.println("GENERAL INFO");
+            out.println("User surname: " + currentStudent.getSurName());
+            out.println("User name: " + currentStudent.getName());
+            out.println("Additional info: " + currentStudent.getGrade());
+            out.println();
+            out.println("TASK COMPLETION INFO");
+            out.println("Start time: " + currentStudent.getStartTime());
+            out.println("End time: " + currentStudent.getFinishTime());
+            out.println("Number of completed tasks: " + currentStudent.getTasksResults().size());
+            out.println("Number of passed tests: " + Integer.parseInt(doneTestField.getText()) + " out of " + currentStudent.getTasksResults().size() * 5);
+            for (int i = 0; i < currentStudent.getTasksResults().size(); i++) {
+                out.println();
+                out.println("TASK #" + (i + 1));
+                out.println("Tests passed: " + currentStudent.getDoneTasks()[i] + " out of 5");
+            }
+            out.println();
+            out.println("User results exported: " + new Date().toString());
+        }
+    }
+
+    private void saveResultsToFile(File targetFile) {
+        boolean isFileOpened = true;
+        if (targetFile != null) {
+            try {
+                targetFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                isFileOpened = false;
+                showError("An error occurred while creating the file!");
+            }
+            if (isFileOpened)
+            {
+                try {
+                    printResults(targetFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    showError("An error occurred while exporting the results!");
+                }
             }
         }
     }
