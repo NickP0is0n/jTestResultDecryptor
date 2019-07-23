440334dc297583517fdd588ba3916999d8467382
NickP0is0n <nickchaywork@gmail.com>
2019-07-23 21:32:08 +0300

    new main form design

--- src/main/java/me/NickP0is0n/Main.java
+++ src/main/java/me/NickP0is0n/Main.java
@@ -2,29 +2,18 @@ package me.NickP0is0n;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import jfxtras.styles.jmetro8.JMetro;
-
-import javax.swing.*;
-import java.io.File;
+import me.NickP0is0n.FormAPI.Form;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ResultsDecryptor.fxml"));
-        primaryStage.setTitle("jTest Result Decryptor");
-        primaryStage.getIcons().add(new Image(new File("resources/logo.png").toURI().toString()));
+        Form mainForm = new Form("ResultsDecryptor.fxml", "jTest Result Decryptor", 524, 415);
         java.awt.Image logo = SwingFXUtils.fromFXImage(new Image(Main.class.getClassLoader().getResourceAsStream("logo.png")), null);
         if(System.getProperty("os.name").equals("Mac OS X")) com.apple.eawt.Application.getApplication().setDockIconImage(logo);
-        primaryStage.setScene(new Scene(root, 730, 400));
-        new JMetro(JMetro.Style.LIGHT).applyTheme(root);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        mainForm.show();
     }
 
 
