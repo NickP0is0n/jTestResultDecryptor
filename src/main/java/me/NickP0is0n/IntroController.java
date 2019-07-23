4adf3edc409c21ff484ab4f1e526faf97b62d08e
NickP0is0n <nickchaywork@gmail.com>
2019-07-23 21:39:57 +0300

    new intro form file

---
+++ src/main/java/me/NickP0is0n/IntroController.java
@@ -0,0 +1,24 @@
+package me.NickP0is0n;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class IntroController {
+
+    @FXML
+    void onAboutBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onExitBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOpenBtn(ActionEvent event) {
+
+    }
+
+}
